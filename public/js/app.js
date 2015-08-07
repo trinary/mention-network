2fa8cb9abd65a9613d2fb789ac00e847f2bd81e3
Erik Cunningham <djtrinary@gmail.com>
2015-08-07 15:26:31 -0600

    ewc: print some stats, think about some zooming/eviction

--- public/js/app.js
+++ public/js/app.js
@@ -16,25 +16,40 @@
   //
   //setup some common vars
   var width = window.innerWidth, // default width
-      height = window.innerHeight; // default height
+      height = window.innerHeight, // default height
+      limit = 30,
+      nodes = [], links = [], indexMap = {}, mentionCount = 0, images = {};
 
-  var nodes = [], links = [], indexMap = {}, mentionCount = 0;
-
-  var canvas = d3.select('body').append('canvas')
+    d3.select('body').append('canvas')
       .attr('width', width)
       .attr('height', height);
-   var context = canvas.node().getContext("2d");
 
-  // build zoom functionality over an entire rectangle not just parts of the graph.
-  var zoom = d3.behavior.zoom().scaleExtent([1, 5]).on('zoom', function () {
-    console.log(d3.event, this);
-    graph_container.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
-  });
+  var canvas = document.getElementsByTagName("canvas")[0];
+  var context = canvas.getContext("2d");
+
+  var lastX=canvas.width/2, lastY=canvas.height/2;
+
+  var zoom = function(clicks) {
+    var pt = context.transformedPoint(lastX,lastY);
+    context.translate(pt.x,pt.y);
+    var factor = Math.pow(scaleFactor,clicks);
+    context.scale(factor,factor);
+    context.translate(-pt.x,-pt.y);
+    tick();
+  };
+
+
+  var handleScroll = function(evt){
+    var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+    if (delta) zoom(delta);
+    return evt.preventDefault() && false;
+  };
+
+  canvas.addEventListener('DOMMouseScroll',handleScroll,false);
+  canvas.addEventListener('mousewheel',handleScroll,false);
 
   // build force layout
   var force = d3.layout.force()
-      .charge(-120)
-      .linkDistance(30)
       .nodes(nodes)
       .links(links)
       .size([width, height])
@@ -46,28 +61,42 @@
       name: tweet.actor.preferredUsername,
       displayName: tweet.actor.displayName,
       id: +(tweet.actor.id.split(':')[2]),
-      image: tweet.actor.image,
+      image: new Image(),
       text: tweet.body,
-      summary: tweet.actor.summary
+      summary: tweet.actor.summary,
+      loaded: false
+    };
+
+    user.image.src = tweet.actor.image;
+    user.image.onload = function() { 
+      user.loaded = true;
+      tick();
     };
 
     if (tweet.twitter_entities.user_mentions.length === 0) { return; }
 
     var mentions = tweet.twitter_entities.user_mentions.map(function (m) {
-      return {name: m.screen_name, displayName: m.name, image: '/img/empty.png', id: m.id};
+      var mentioned = {
+        name: m.screen_name,
+        displayName: m.name,
+        image: new Image(),
+        id: m.id,
+        loaded: false
+      };
+
+      return mentioned;
     });
 
     if (! indexMap[user.id]) {
       nodes.push(user);
       indexMap[user.id] = { index: nodes.length - 1, links: [] };
     } else {
-      if (nodes[indexMap[user.id].index].image === '/img/empty.png') {
-        // we have new info for this user.
-        nodes[indexMap[user.id].index].image = user.image;
-        nodes[indexMap[user.id].index].summary = user.summary;
-        nodes[indexMap[user.id].index].text = tweet.body;
-      }
+      nodes[indexMap[user.id].index].image = user.image;
+      nodes[indexMap[user.id].index].summary = user.summary;
+      nodes[indexMap[user.id].index].text = tweet.body;
     }
+
+    //evict(indexMap);
 
     for (var i = 0; i < mentions.length; i++) {
       var m = mentions[i];
@@ -81,16 +110,23 @@
       if (m.id !== user.id && indexMap[user.id].links.indexOf(m.id) === -1) {
         indexMap[user.id].links.push(m.id);
         links.push({source: nodes[indexMap[user.id].index], target: nodes[indexMap[m.id].index], value: 1 });
-        console.log(new Date() + 'pushing link between ' + indexMap[user.id].index + ' and ' + indexMap[m.id].index + '. ids: ' + [user.id, m.id]);
-      } else {
-        console.log('self-link or existing link detected between ' + indexMap[user.id].index + ' and ' + indexMap[m.id].index + '. ids: ' + [user.id, m.id]);
       }
+
       force
         .nodes(nodes)
         .links(links)
         .start();
     }
   });
+
+  function evict(indexMap) {
+    var randomIndex, randomUser, keys = Object.keys(indexMap);
+
+    while(nodes.length > limit) {
+      randomUser = indexMap[randomIndex];
+      delete nodes[randomUser.index];
+    }
+  }
 
   function tick() {
     context.clearRect(0, 0, width, height);
@@ -105,12 +141,23 @@
     context.stroke();
 
     // draw nodes
-    context.fillStyle = "steelblue";
     context.beginPath();
+    context.fillStyle = "#55acee";
     nodes.forEach(function(d) {
       context.moveTo(d.x, d.y);
-      context.arc(d.x, d.y, 4.5, 0, 2 * Math.PI);
+      if (d.loaded) {
+        context.drawImage(d.image, d.x - 6, d.y - 6, d.image.width * 0.25, d.image.height * 0.25);
+      } else {
+        context.fillRect(d.x - 6 , d.y -6, 12, 12);
+      }
     });
+
+    // draw stats
+    context.fillStyle = '#ccc';
+    context.font = 'bold 30px sans-serif';
+    context.textBaseline = 'bottom';
+    context.fillText("Nodes: " + nodes.length, 40, 40);
+    context.fillText("Edges: " + links.length, 40, 80);
     context.fill();
   }
 }());
